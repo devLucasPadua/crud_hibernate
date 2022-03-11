3c0c46b94531f3272a5af511ba39c9455dcf1dfe
Lucas Ribeiro de Padua <lucaspadua@franca.intranet>
2022-03-11 16:22:09 -0300

    Initial commit

---
+++ src/main/java/com/lucaspadua/crud_hibernate/CrudHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.lucaspadua.crud_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucaspadua/crud_hibernate/CrudHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaspadua.crud_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
